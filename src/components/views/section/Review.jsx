91443079e21451ee06e3397a6cf66989be3c5a6f
5eokhwan <exist909@naver.com>
2022-01-18 21:59:22 +0900

    더보기 기능 추가

--- src/components/views/section/Review.jsx
+++ src/components/views/section/Review.jsx
@@ -1,11 +1,42 @@
+import React, { useRef, useState, useEffect } from "react";
+
+function checkOverflow(el) {
+  let curOverflow = el.style.overflow;
+
+  if (!curOverflow || curOverflow === "visible") el.style.overflow = "hidden";
+
+  let isOverflowing =
+    el.clientWidth < el.scrollWidth || el.clientHeight < el.scrollHeight;
+
+  el.style.overflow = curOverflow;
+
+  return isOverflowing;
+}
+
 function Review({ info }) {
+  const [isSpread, SetIsSpread] = useState(false);
+  const textBoxRef = useRef();
+  let isSpreadBtn = false;
+  if (textBoxRef.current) isSpreadBtn = checkOverflow(textBoxRef.current);
+
   return (
-    <article>
+    <article className={isSpread ? "more" : ""}>
       <h1>
         {info.student_name} | {info.year_name + ", " + info.subject_view_name}
       </h1>
       <p className="date">{info.reg_datetime}</p>
-      <p className="content">{info.review}</p>
+      <div className="content" ref={textBoxRef}>
+        {info.review}
+      </div>
+
+      <div
+        className="more_button"
+        onClick={() => {
+          SetIsSpread(!isSpread);
+        }}
+      >
+        {isSpreadBtn ? (isSpread ? "축소" : "더보기") : null}
+      </div>
     </article>
   );
 }

ed7dae32435cc700b7b6369417ed1db5401f657f
exist909@naver.com <exist909@naver.com>
2022-03-31 11:12:06 +0900

    testData

--- src/components/views/section/Reviews.jsx
+++ src/components/views/section/Reviews.jsx
@@ -4,7 +4,7 @@ import Axios from "axios";
 import Loading from "../../global/Loading";
 import Review from "./Review";
 import { renewTotalData } from "../../../reducers/page";
-
+import { data } from "./testData";
 function Reviews() {
   const dispatch = useDispatch();
   const { size, offset } = useSelector(
@@ -24,15 +24,17 @@ function Reviews() {
         filters[1].names[filters[1]["active"]] !== "전체"
           ? `&SUBJECT=${filters[1].names[filters[1]["active"]]}`
           : "";
-
-      const response = await Axios.get(
+      const reqUri =
         `https://dev.seoltab.com/front_test_review?SIZE=${size}&OFFSET=${offset}` +
-          yearFilterQueryStr +
-          subjectFilterQueryStr
-      );
-      console.log(response.data);
-      setReviews(response.data.data);
-      dispatch(renewTotalData(response.data.total));
+        yearFilterQueryStr +
+        subjectFilterQueryStr;
+      // const response = await Axios.get(reqUri);
+      //현재 백엔드가 작동하지 않으므로 테스트 데이터를 넣었습니다.
+      //원래는 필터가 변할 때마다 쿼리 파라미터값 변경 후 서버에 재요청하여 새로운 필터링 된 데이터를 받아와서 렌더링합니다.
+      console.log("요청: " + reqUri);
+      const response = data;
+      setReviews(response.data);
+      dispatch(renewTotalData(response.total));
     }
     fetchData();
   }, [filters, offset]);
